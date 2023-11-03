00897da0984c136ebcd69d8928c1020607f25d3d
takato-sakuma <takato_sakuma@nuco.co.jp>
2023-11-04 08:44:50 +0900

    .

--- web_app/bin/render-build.sh
+++ web_app/bin/render-build.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
+
 bundle install
+yarn install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails webpacker:install
+bundle exec rails webpacker:compile
 bundle exec rails db:migrate
