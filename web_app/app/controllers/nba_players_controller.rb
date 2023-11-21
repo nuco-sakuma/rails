769a65fd59c17e256d7bd36e372d8a078c2a00dc
takato-sakuma <takato_sakuma@nuco.co.jp>
2023-11-21 22:14:48 +0900

    revise calculate

--- web_app/app/controllers/nba_players_controller.rb
+++ web_app/app/controllers/nba_players_controller.rb
@@ -16,16 +16,16 @@ class NbaPlayersController < ApplicationController
     #文字列を数字に変換
     new_sum_score = params[:nba_player][:sum_score].to_i
     @nba_player.sum_score += new_sum_score
-    
+
+    if @nba_player.post_count>0
+      @nba_player.score =@nba_player.sum_score / (@nba_player.post_count+1)
+    else
+      @nba_player.score = @nba_player.sum_score
+    end
     
     # 認可されたパラメータ:sum_score(private以下)に現在の@nba_player.sum_scoreを追加している
     if @nba_player.update(nba_player_params.merge(sum_score: @nba_player.sum_score))
       @nba_player.increment!(:post_count)
-      if @nba_player.post_count>0
-        @nba_player.score =@nba_player.sum_score / @nba_player.post_count
-      else
-        @nba_player.score = @nba_player.sum_score
-      end
       flash[:success] = "Profile updated"
       redirect_to @nba_player
     else
